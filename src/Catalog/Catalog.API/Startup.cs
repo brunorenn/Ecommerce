ba5292c527fa0ece483021da325bd13bd2607a97
Bruno Lima <brunorenn@outlook.com.br>
2018-06-26 01:25:30 -0300

    Criação do arquivo startupcs

--- src/Catalog/Catalog.API/Startup.cs
+++ src/Catalog/Catalog.API/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Reflection;
 
@@ -25,6 +26,57 @@ namespace Catalog.API
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
+            ConfigureSwagger(services);
+            ConfigureDatabase(services);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                //Hsts protocol force https all requests
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+            ConfigureSwaggerUI(app, env, loggerFactory);
+        }
+
+        public void ConfigureSwagger(IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                options.DescribeAllEnumsAsStrings();
+
+                options.SwaggerDoc("v1", new Swashbuckle.AspNetCore.Swagger.Info
+                {
+                    Title = "Ecommerce - catalog API document",
+                    Version = "v1",
+                    Description = "The Catalog Microservice HTTP API",
+                    TermsOfService = "Terms Of Service"
+                });
+            });
+        }
+
+        public void ConfigureSwaggerUI(IApplicationBuilder app,
+                             IHostingEnvironment env,
+                             ILoggerFactory loggerFactory)
+        {
+            app.UseSwagger()
+            .UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog.API V1");
+            });
+        }
+
+        public void ConfigureDatabase(IServiceCollection services)
+        {
             services.AddDbContext<CatalogContext>(options =>
             {
                 options.UseSqlServer(Configuration["ConnectionString"], sqlServerOptionsAction: sqlOptions =>
@@ -48,23 +100,6 @@ namespace Catalog.API
                     options.ConfigureWarnings(warnings => warnings.Throw(RelationalEventId.QueryClientEvaluationWarning));
                 });
             });
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                //Hsts protocol force https all requests
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseMvc();
         }
     }
 }
